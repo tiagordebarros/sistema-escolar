20163368f9c35ca529195c2d5a5ae5533bf5dc40
Tiago Ribeiro de Barros <90200916+tiagordebarros@users.noreply.github.com>
2024-04-03 17:15:15 -0300

    feature: create class ListaDeAlunos

--- src/Aluno.java
+++ src/Aluno.java
@@ -5,4 +5,11 @@ public class Aluno {
   String cpf;
   String endereco;
   LocalDate dataDeNascimento;
+
+  public Aluno(String nome, String cpf, String endereco, LocalDate dataDeNascimento) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.endereco = endereco;
+    this.dataDeNascimento = dataDeNascimento;
+  }
 }
--- src/ListaDeAlunos.java
+++ src/ListaDeAlunos.java
@@ -1,3 +1,13 @@
+import java.time.LocalDate;
+import java.util.Arrays;
+
 public class ListaDeAlunos {
-  
+  public static void main(String[] args) {
+    Aluno[] alunos = {
+      new Aluno("John Doe", "123.456.789-00","Rua Sem Nome",LocalDate.of(1990,11,12)), 
+      new Aluno("Joana Doe","321.654.987-11","Rua Sem Número",LocalDate.of(2000,06,10))
+    };
+
+    System.out.println(Arrays.toString(alunos));
+  }
 }
