ad41e35b3d90e952b4baadcd8dec34b9dac0272e
Tiago Ribeiro de Barros <90200916+tiagordebarros@users.noreply.github.com>
2024-04-04 17:49:56 -0300

    refactor: add Constructor to Turma class

--- src/Turma.java
+++ src/Turma.java
@@ -1,7 +1,18 @@
+import java.util.List;
+
 public class Turma {
   int codigoDaTurma;
   String etapaDeEnsino;
   int ano;
   int limiteDeVagas;
   int numeroDeMatriculados;
+  List<Aluno> alunosMatriculados;
+
+  public Turma(int codigoDaTurma, String etapaDeEnsino, int ano, int limiteDeVagas, int numeroDeMatriculados) {
+    this.codigoDaTurma = codigoDaTurma;
+    this.etapaDeEnsino = etapaDeEnsino;
+    this.ano = ano;
+    this.limiteDeVagas = limiteDeVagas;
+    this.numeroDeMatriculados = numeroDeMatriculados;
+  }
 }
