9c72c3abc297e3aae2a0207bf1ac8c21e4618a08
Tiago Ribeiro de Barros <90200916+tiagordebarros@users.noreply.github.com>
2024-04-06 16:30:40 -0300

    feature: create class InterfaceDoUsuario and add initial menu options

--- src/InterfaceDoUsuario.java
+++ src/InterfaceDoUsuario.java
@@ -1,3 +1,70 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class InterfaceDoUsuario {
-  
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Lista de alunos
+        ListaDeAlunos listaDeAlunos = new ListaDeAlunos();
+
+        // Lista de turmas
+        List<Turma> turmas = new ArrayList<>();
+
+        // Menu de opções
+        int opcao;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Cadastrar Aluno");
+            System.out.println("2. Cadastrar Turma");
+            System.out.println("3. Matricular Aluno em Turma");
+            System.out.println("4. Listar Alunos em Ordem Alfabética");
+            System.out.println("5. Listar Turmas");
+            System.out.println("6. Listar Alunos Matriculados em uma Turma");
+            System.out.println("7. Listar Alunos Fora da Idade Prevista de Uma Etapa");
+            System.out.println("8. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();  // Limpar o buffer do scanner
+
+            switch (opcao) {
+                case 1:
+                    // Cadastrar aluno
+                    System.out.print("Nome do aluno: ");
+                    String nome = scanner.nextLine();
+                    System.out.print("CPF do aluno: ");
+                    String cpf = scanner.nextLine();
+                    System.out.print("Endereço do aluno: ");
+                    String endereco = scanner.nextLine();
+                    System.out.print("Data de nascimento do aluno (AAAA-MM-DD): ");
+                    String dataNascimentoStr = scanner.nextLine();
+                    LocalDate dataNascimento = LocalDate.parse(dataNascimentoStr);
+                    Aluno aluno = new Aluno(nome, cpf, endereco, dataNascimento);
+                    listaDeAlunos.incluirNoFim(aluno);
+                    break;
+                case 2:
+                    // Cadastrar turma
+                    System.out.print("Código da turma: ");
+                    int codigoDaTurma = scanner.nextInt();
+                    scanner.nextLine();  // Limpar o buffer do scanner
+                    System.out.print("Etapa de ensino (infantil, fundamental anos iniciais, fundamental anos finais, médio): ");
+                    String etapaDeEnsino = scanner.nextLine();
+                    System.out.print("Ano: ");
+                    int ano = scanner.nextInt();
+                    System.out.print("Limite de vagas: ");
+                    int limiteDeVagas = scanner.nextInt();
+                    System.out.print("Número de Matriculados: ");
+                    int numeroDeMatriculados = scanner.nextInt();
+                    Turma turma = new Turma(codigoDaTurma, etapaDeEnsino, ano, limiteDeVagas, numeroDeMatriculados);
+                    turmas.add(turma);
+                    break;
+
+                    default:
+                    System.out.println("Opção inválida.\n");
+            }
+        } while (opcao != 7);
+        scanner.close();
+    }
 }
--- src/ListaDeAlunos.java
+++ src/ListaDeAlunos.java
@@ -1,7 +1,11 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ListaDeAlunos {
+  private List<Aluno> alunos;
+  // private Aluno[] alunos;
   public static void main(String[] args) {
     Aluno[] alunos = {
       new Aluno("John Doe", "123.456.789-00","Rua Sem Nome",LocalDate.of(1990,11,12)), 
@@ -10,4 +14,23 @@ public class ListaDeAlunos {
 
     System.out.println(Arrays.toString(alunos));
   }
+
+  public ListaDeAlunos() {
+    this.alunos = new ArrayList<>();
+}
+  
+  // inclui um Aluno no início da lista
+  // public void incluirNoInicio(Aluno aluno) {
+  //   Aluno[] novaLista = new Aluno[alunos.length + 1];
+  //   novaLista[0] = aluno;
+  //   System.arraycopy(alunos, 0, novaLista, 1, alunos.length);
+  //   alunos = novaLista;
+  // }
+
+  public void incluirNoFim(Aluno aluno) {
+    alunos.add(aluno);
+    System.out.println(alunos);
+    System.out.println("Aluno cadastrado com sucesso!");
+  }
+
 }
