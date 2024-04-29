c81078691d5d75c229a34183fd4f0d3b707104e4
Tiago Ribeiro de Barros <90200916+tiagordebarros@users.noreply.github.com>
2024-04-29 17:28:38 -0300

    refactor: implement static method to initialize the program from the main class

--- src/InterfaceDoUsuario.java
+++ src/InterfaceDoUsuario.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InterfaceDoUsuario {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Lista de alunos
